2880485683101918905a4afbafd50fce0eaf434c
DEV_T <tewsk132@gmail.com>
2024-08-25 15:39:35 +0700

    update install jwt

---
+++ API/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using Application.Features.BlogPosts.Queries.GetAllBlogPosts;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMediator mediator;
+        public AuthController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register()
+        {
+            //var blogPosts = await mediator.Send(new GetAllBlogPostsQuery());
+            return Ok();
+        }
+    }
+}
