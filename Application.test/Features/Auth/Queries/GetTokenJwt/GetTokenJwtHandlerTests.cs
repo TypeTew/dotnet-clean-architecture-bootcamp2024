be09107975de65f3784145c2308b90adba27f1d1
DEV_T <tewsk132@gmail.com>
2024-08-31 10:55:14 +0700

    update unit test get token

--- Application.test/Features/Auth/Queries/GetTokenJwt/GetTokenJwtHandlerTests.cs
+++ Application.test/Features/Auth/Queries/GetTokenJwt/GetTokenJwtHandlerTests.cs
@@ -1,6 +1,8 @@
 ﻿using Application.Features.Auth.Queries.GetTokenJwt;
-using Castle.Core.Configuration;
+
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+
 using Moq;
 
 namespace Application.test.Features.Auth.Queries.GetTokenJwt
@@ -13,46 +15,98 @@ namespace Application.test.Features.Auth.Queries.GetTokenJwt
         public async Task Handle_InvalidEmail_ShouldReturnsNull()
         {
             //
+            //GetTokenJwtQuery request = new()
+            //{
+            //    Email = "test@gmail.com",
+            //    Password = "password"
+            //};
+
+            //var user = new IdentityUser
+            //{
+            //    Email = request.Email,
+            //};
+
+            //CancellationToken cancellationToken = CancellationToken.None;
+
+            ////var userManagerMock = new Mock<UserManager<IdentityUser>>();
+            //var userStoreMock = new Mock<IUserStore<IdentityUser>>();
+            //var userManagerMock = new Mock<UserManager<IdentityUser>>(
+            //    userStoreMock.Object, 
+            //    default, 
+            //    default,
+            //    default, 
+            //    default, 
+            //    default, 
+            //    default,
+            //    default, 
+            //    default
+            //    );
+
+
+            //userManagerMock.Setup(it => it.FindByEmailAsync(request.Email))
+            //                    .Returns(Task.FromResult<IdentityUser>(null));
+
+            //var mockConfiguration = new Mock<IConfiguration>();
+
+            //var handler = new GetTokenJwtHandler(userManagerMock.Object,
+            //                (Microsoft.Extensions.Configuration.IConfiguration)mockConfiguration.Object);
+
+            //var result = await handler.Handle(request, cancellationToken);
+            ////Act
+
+            //Assert.Null(userManagerMock.Object);
+
+
+            // Arrange
             GetTokenJwtQuery request = new()
             {
-                Email = "test@gmail.com",
-                Password = "password"
+                Email = "someone@somewhere.com"
             };
-
-            var user = new IdentityUser
-            {
-                Email = request.Email,
-            };
-
             CancellationToken cancellationToken = CancellationToken.None;
-
-            //var userManagerMock = new Mock<UserManager<IdentityUser>>();
             var userStoreMock = new Mock<IUserStore<IdentityUser>>();
-            var userManagerMock = new Mock<UserManager<IdentityUser>>(
-                userStoreMock.Object, 
-                default, 
-                default,
-                default, 
-                default, 
-                default, 
-                default,
-                default, 
-                default
-                );
-
-
-            userManagerMock.Setup(it => it.FindByEmailAsync(request.Email))
-                                .Returns(Task.FromResult<IdentityUser>(null));
-
+            var mockUserManager = new Mock<UserManager<IdentityUser>>(
+                userStoreMock.Object, default, default, default, default, default, default, default, default);
+            mockUserManager.Setup(it => it.FindByEmailAsync(request.Email))
+                .Returns(Task.FromResult<IdentityUser>(null));
             var mockConfiguration = new Mock<IConfiguration>();
 
-            var handler = new GetTokenJwtHandler(userManagerMock.Object,
-                            (Microsoft.Extensions.Configuration.IConfiguration)mockConfiguration.Object);
-
+            var handler = new GetTokenJwtHandler(mockUserManager.Object, mockConfiguration.Object);
+            // Act
             var result = await handler.Handle(request, cancellationToken);
-            //Act
-
-            Assert.Null(userManagerMock.Object);
+            // Assert
+            Assert.Null(result);
         }
+
+        [Fact]
+        public async Task Handle_CorrectEmailInvalidPassword_ShouldReturnsNull()
+        {
+            // Arrange
+            GetTokenJwtQuery request = new()
+            {
+                Email = "someone@somewhere.com",
+                Password = "password"
+            };
+            var user = new IdentityUser
+            {
+                Email = request.Email
+            };
+            CancellationToken cancellationToken = CancellationToken.None;
+            var userStoreMock = new Mock<IUserStore<IdentityUser>>();
+            var mockUserManager = new Mock<UserManager<IdentityUser>>(
+                userStoreMock.Object, default, default, default, default, default, default, default, default);
+            mockUserManager.Setup(it => it.FindByEmailAsync(request.Email))
+                .Returns(Task.FromResult(user));
+            mockUserManager.Setup(it => it.CheckPasswordAsync(user, It.IsAny<string>()))
+                .Returns(Task.FromResult(false));
+            var mockConfiguration = new Mock<IConfiguration>();
+
+            var handler = new GetTokenJwtHandler(mockUserManager.Object, mockConfiguration.Object);
+            // Act
+            var result = await handler.Handle(request, cancellationToken);
+            // Assert
+            Assert.Null(result);
+
+        }
+
     }
 }
